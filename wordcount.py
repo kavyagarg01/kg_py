82c34839219a1f55fe02ec4f5a14c0094e4a5868
kagarg <kavya.garg@geminisolutions.in>
2020-03-30 22:22:48 +0530

    Word-Count Python Program

---
+++ wordcount.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python
+import sys
+filename=sys.argv[1]
+f1 = open(filename,'r')
+ls= f1.read().split(' ')
+ls =sorted(ls)
+print ls
+prev=ls[0]
+count =0
+flag=1
+d={}
+for i in ls[:len(ls)-1]:
+ current =i
+ if count > 0:
+  if prev == current:
+    flag = flag + 1
+  d[prev]=flag
+ prev=i
+ count = count + 1
+print d
